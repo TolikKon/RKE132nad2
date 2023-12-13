b7a9e3855ff0afadc39bb74da1878e2dc2e2bfa6
Anatoly <anatoly.kondrat@tktk.ee>
2023-12-13 10:36:19 +0200

    first commit

---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Enter your age:");
+
+
+int userAge = Int32.Parse(Console.ReadLine());
+
+
+
+if(userAge >= 13)
+
+{
+    Console.WriteLine("Welcome to Instagram!");
+
+} 
+else //if (userAge < 13)
+{
+    Console.WriteLine("You are too young to use Instagram.");
+
+}
